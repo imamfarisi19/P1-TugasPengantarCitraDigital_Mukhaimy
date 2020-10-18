39d29fa39c22dffe9365c7e7f5b34406dc37dac4
imam farisi <edwardindo1@gmail.com>
2020-10-19 01:43:37 +0800

    Initial commit

---
+++ P1-VCCD-MS-Half_Grayscale_invert/HalfGrayscaleInvert.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1_VCCD_MS_Half_Grayscale_invert
+{
+   public partial class HalfGrayscaleInvert : Form
+   {
+      public HalfGrayscaleInvert()
+      {
+         InitializeComponent();
+      }
+      Bitmap newBitmap;
+      Image file;
+      Boolean opened = false;
+      private void button1_Click(object sender, EventArgs e)
+      {
+         DialogResult dr = openFileDialog1.ShowDialog();
+
+         if (dr == DialogResult.OK)
+         {
+            file = Image.FromFile(openFileDialog1.FileName);
+            newBitmap = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = file;
+            opened = true;
+         }
+      }
+
+      private void button2_Click(object sender, EventArgs e)
+      {
+         if (opened == true)
+         {
+            for (int x = 0; x < newBitmap.Width; x++)
+            {
+               for (int y = 0; y < newBitmap.Height; y++)
+               {
+                  if (x <= (newBitmap.Width / 2))
+                  {
+                     //change to Grayscale color
+                     Color originalColor = newBitmap.GetPixel(x, y);
+                     int grayScale = (int)((originalColor.R * .3) + (originalColor.G * .59) + (originalColor.B * .11));
+                     Color newColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                     newBitmap.SetPixel(x, y, newColor);
+                  }
+                  else if (x >= (newBitmap.Width / 2))
+                  {
+                     //change to invert color
+                     Color pixel = newBitmap.GetPixel(x, y);
+                     int red = pixel.R;
+                     int green = pixel.G;
+                     int blue = pixel.B;
+                     newBitmap.SetPixel(x, y, Color.FromArgb(255 - red, 255 - green, 255 - blue));
+                  }
+               }
+            }
+            pictureBox2.Image = newBitmap;
+         }
+         else
+         {
+            MessageBox.Show("Please! upload an image", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+         }
+      }
+
+      private void label2_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void label1_Click(object sender, EventArgs e)
+      {
+
+      }
+   }
+}
---
+++ P1-VCCD-MS-MakeRandomBoxOfColor/MakeRandomBoxOfColor.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1_VCCD_MS_MakeRandomBoxOfColor
+{
+   public partial class MakeRandomBoxOfColor : Form
+   {
+      public MakeRandomBoxOfColor()
+      {
+         InitializeComponent();
+      }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         int width = 500, height = 100;
+
+         //bitmap
+         Bitmap bmp = new Bitmap(width,height);
+
+         //random number
+         Random rand = new Random();
+
+         //create random pixels
+         for (int y = 0; y < height; y++)
+         {
+            for (int x = 0; x < width; x++)
+            {
+               //generate and set ARGB value 
+               if (x <= 100)
+               {
+                  int a = textBox1.Text;
+                  int r = 255;
+                  int g = 0;
+                  int b = 0;
+                  bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+               }
+               else if (x <= 200)
+               {
+                  int a = 250;
+                  int r = 0;
+                  int g = 255;
+                  int b = 0;
+                  bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+               }
+               else if (x <= 300)
+               {
+                  int a = 250;
+                  int r = 0;
+                  int g = 0;
+                  int b = 255;
+                  bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+               }
+               else if (x <= 400)
+               {
+                  int a = 250;
+                  int r = 0;
+                  int g = 234;
+                  int b = 255;
+                  bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+               }
+               else if (x <= 500)
+               {
+                  int a = 250;
+                  int r = 145;
+                  int g = 0;
+                  int b = 255;
+                  bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+               }
+            }
+         }
+
+         //load bmp in picturebox1
+         pictureBox1.Image = bmp;
+
+         //save (write) random pixel image
+         bmp.Save("C:\\Users\\USER\\Downloads\\Hasil\\RandomImageOfColor.png");         
+        }
+
+      private void pictureBox1_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void label20_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void label19_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void label18_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void label17_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void textBox1_TextChanged(object sender, EventArgs e)
+      {
+         if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+         {
+            e.Handled = true;
+         }
+
+         // only allow one decimal point
+         if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+         {
+            e.Handled = true;
+         }
+      }
+   }
+}
